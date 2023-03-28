9cbf0f1f86eee0fb5c9cdee204238fffc7b309fa
Babarali345 <babar@tecizeverything.com>
2023-03-28 09:38:04 +0500

    Fix default page

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors=require('cors')
 const app = express();
 //1 MIDDELE_WARE
 app.use(cors({
-  origin:'http://127.0.0.1:3000'
+  origin:'http://127.0.0.1:3000/'
 }))
 app.use(express.json());
 if (process.env.NODE_ENV === "development") {
@@ -19,6 +19,9 @@ const userRouter = require("./routes/userRouters");
 const globalErrorHandler = require("./controller/errorController");
 
 app.use("/api/v1/user", userRouter);
+app.use("/", (req,res)=>{
+  res.send('Hellow world')
+})
 
 app.all('*',(req,res,next)=>{
   res.status(404).json({
